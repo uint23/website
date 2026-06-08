90d33a9296085fd9489055b8e7ca20ce4a92f67a
uint <uint@fent.uk>
2026-06-08 17:49:42 +0100

    rebrand to netcat.uk

--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 package main
 
 const SiteTitle = "fent.uk"
-const FooterText = "made with <a href=\"https://github.com/uint23/kew\">kew</a>"
+const FooterText = "nc::uk < <a href=\"https://github.com/uint23/kew\">kew</a>"
 const TemplateFile = "template.html"
 const NavDirSymbol = "/"
 const NavFileSymbol = ""
--- site/Contact.md
+++ site/Contact.md
@@ -1,5 +1,5 @@
 ### Contact
 
 GitHub: [https://github.com/uint23](https://github.com/uint23)  
-Email: [uint@fent.uk](mailto:uint@fent.uk)
+Email: [unsigned@netcat.uk](mailto:unsigned@netcat.uk)
 
