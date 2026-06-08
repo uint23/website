03d6e5a2fef7a1c3ea6984bee53927697382a842
uint <uint@fent.uk>
2026-06-08 20:17:26 +0100

    add webring image

--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 package main
 
 const SiteTitle = "fent.uk"
-const FooterText = "nc::uk < <a href=\"https://github.com/uint23/kew\">kew</a>"
+const FooterText = ""
 const TemplateFile = "template.html"
 const NavDirSymbol = "/"
 const NavFileSymbol = ""
--- site/index.md
+++ site/index.md
@@ -1,4 +1,6 @@
 ### Willkommen!
 
-Navigate using the navbar.
+Navigate using the navbar.  
+This website is also generated with [kew!](https://github.com/uint23/kew)--my
+own static site generator.
 
